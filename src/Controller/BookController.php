4e65389b62c4dd9711491280dd2ee81ee9616a28
Arnaud BODEL <contact@osw3.net>
2021-04-13 16:15:05 +0200

    Ajoute des controlle homepage,book,support

---
+++ src/Controller/BookController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    // Affiche la liste des livres
+    // Index
+    /**
+     * @Route("/books", name="book")
+     */
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+        ]);
+    }
+
+
+    // Creation d'un livre dans la bdd
+    // Create
+    // ...
+
+
+    // Affiche le détail d'un livre
+    // Read / Retrieve
+    // ... 
+
+
+    // Modifier un livre
+    // Update
+    // ... 
+
+
+    // Supprimer un livre
+    // Delete
+    // ...
+}
---
+++ src/Controller/SupportController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SupportController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('support/contact.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/faq", name="faq")
+     */
+    public function faq(): Response
+    {
+        return $this->render('support/faq.html.twig', [
+        ]);
+    }
+}
