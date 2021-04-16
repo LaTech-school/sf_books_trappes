f1157ef555d246992d437cca3a482d378498fa50
Arnaud BODEL <contact@osw3.net>
2021-04-16 10:40:08 +0200

    -

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,12 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import './../node_modules/remixicon/fonts/remixicon.css';
+import 'remixicon/fonts/remixicon.css';
+
 
 // import './styles/app.css';
 
